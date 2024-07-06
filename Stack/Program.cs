13f77db4aba40a6321f472f89d8fe3cec3ef4dc2
ebrah <ebrah@LAPTOP-TM8TPK8A>
2024-07-06 10:46:59 +0300

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,74 @@
+﻿using Stack;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stk s = new Stk();
+        int Choice;
+        int value;
+        do 
+        {
+            showOptions();
+            Console.WriteLine("Enter your choice");
+            Choice = Convert.ToInt32(Console.ReadLine());
+            switch (Choice) 
+            {
+                case 0:
+                    Console.WriteLine("Bye");
+                    break;
+                case 1:
+                    if (s.IsFull())
+                    {
+                        Console.WriteLine("Stack over flow");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not full");
+                    }
+                    break;
+                case 2:
+                    if (s.IsEmpty())
+                    {
+                        Console.WriteLine("Already Empty");
+                    }
+                    else
+                    {
+                        Console.WriteLine("there are elements");
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Enter value to push it");
+                    value = Convert.ToInt32(Console.ReadLine());
+                    s.push(value);
+                    break;
+                case 4:
+                    s.pop();
+                    break;
+                case 5:
+                    Console.WriteLine("Enter your value");
+                    value = Convert.ToInt32(Console.ReadLine());
+                    s.search(value);
+                    break;
+                case 6:
+                    s.Display();
+                    break;
+                default:
+                    Console.WriteLine("invaild option");
+                    break;
+            }
+        } while (Choice != 0);
+    }
+    public static void showOptions() 
+    {
+        Console.WriteLine("___________________________");
+        Console.WriteLine("1-Is full");
+        Console.WriteLine("2-Is empty");
+        Console.WriteLine("3-push");
+        Console.WriteLine("4-pop");
+        Console.WriteLine("5-search");
+        Console.WriteLine("6-display");
+        Console.WriteLine("0-Exit");
+        Console.WriteLine("___________________________");
+    }
+}
---
+++ Stack/Stk.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+
+namespace Stack
+{
+    public class Stk
+    {
+        int Top;
+        int Size;
+        int[] s;
+        public Stk()
+        {
+            Top = 0;
+            Size = 10;
+            s = new int[Size];
+        }
+        public bool IsFull()
+        {
+            return (Top + 1 <= Size) ? false : true;
+        }
+        public bool IsEmpty()
+        {
+            return (Top == 0) ? true : false;
+        }
+        public void push(int value)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack overFlow");
+            }
+            else
+            {
+                s[Top] = value;
+                Console.WriteLine("Pushed successfully");
+                Top++;
+            }
+        }
+        public void pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Already Empty");
+            }
+            else
+            {
+                s[Top - 1] = 0;
+                Console.WriteLine("poped successfully");
+                Top--;
+            }
+        }
+        
+        public void search(int value) 
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Already Empty");
+            }
+            else
+            {
+                int test = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i]==value)
+                    {
+                        Console.WriteLine("Founded");
+                        return;
+                    }
+                }
+                if (test==0)
+                {
+                    Console.WriteLine("Not Found");
+                }
+            }
+        }
+        public void Display() 
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Already Empty");                
+            }
+            else
+            {
+                for (int i = 0; i < Top; i++)
+                {
+                    Console.WriteLine(s[i]);
+                }
+            }
+        }
+    }
+}
